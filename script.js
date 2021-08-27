f3bec6a4dd8d1ee76591594b56c41ef93a67b87c
Tiago Rodrigues <rtiago450@gmail.com>
2021-08-27 18:25:27 -0300

    adicionado o style.css, mudanças de texto no html e mudança nos comentários do javascript

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Write a function hypotenuse that calculates the length of the hypotenuse of a right triangle. The length of the two legs is passed to the function. Tip:
-// In a right triangle the Pythagorean theorem is valid. If a and b are the lengths of the two legs and c is the length of the hypotenuse,
+// Write a function hypotenuse that calculates the length of the hypotenuse of a right triangle. The length of the two legs is passed to the function. 
+// Tip: In a right triangle the Pythagorean theorem is valid. If a and b are the lengths of the two legs and c is the length of the hypotenuse,
 // the following is true: a² + b² = c². Since 3² + 4² = 5² applies, hypotenuse(3, 4) should return 5.
-
 
 function hypotenuse(side1, side2){
     let side3 = Math.pow(side1, 2) + Math.pow(side2, 2);
     console.log(Math.sqrt(side3)); 
 }
-
 console.log(hypotenuse(2,3));
+
 
 // Write a function midrange, that calculates the midrange of 3 numbers. The midrange is the mean of the smallest and largest number.
 
@@ -17,8 +16,8 @@ function midrange(num1, num2, num3){
     let max = Math.max(num1, num2, num3);
     return (min + max) / 2
  }
+ console.log(midrange(10, 20, 17));
 
- console.log(midrange(10, 20, 17))
 
  // Write a function area that calculates the area of a circle. The function is given the radius of the circle.
 
@@ -35,8 +34,8 @@ function round100(num){
     result = Math.round(num / 100) * 100;
     return result;
 }
-
 console.log(round100(49.999));
+
 
 // Write a function dice that returns like a dice a random number between 1 and 6.
 
@@ -44,10 +43,11 @@ function dice(){
     let x = Math.round( Math.random() * 5 + 1);
     return x;
 }
+console.log(dice());
 
-console.log(dice())
 
-//Write a function add that takes a string with a summation task and returns its result as a number. Two natural numbers should be added.
+// Write a function add that takes a string with a summation task and returns its result as a number. 
+// Two natural numbers should be added.
 // The summation task is a string of the form '102+17'.
 
 function add(string){
@@ -58,7 +58,6 @@ function add(string){
 
     return sum + sum2;
  }
-
 console.log(add('12+5'));
 
 
